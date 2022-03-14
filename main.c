4ad9beb3ca250d54d984dd9850f9098544ba3694
AldinIbrahimovic90 <97173998+AldinIbrahimovic90@users.noreply.github.com>
2022-03-14 15:44:36 +0100

    read and write files

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdlib.h>
 #include <string.h>
 char end(char si, char (*ptab)[3]){
     if( (ptab[0][0]==si&&ptab[0][1]==si&&ptab[0][2]==si)||(ptab[1][0]==si&&ptab[1][1]==si&&ptab[1][2]==si)||(ptab[2][0]==si&&ptab[2][1]==si&&ptab[2][2]==si)||(ptab[0][0]==si&&ptab[1][0]==si&&ptab[2][0]==si)||(ptab[0][1]==si&&ptab[1][1]==si&&ptab[2][1]==si)||(ptab[0][2]==si&&ptab[1][2]==si&&ptab[2][2]==si)||(ptab[0][0]==si&&ptab[1][1]==si&&ptab[2][2]==si)||(ptab[0][2]==si&&ptab[1][1]==si&&ptab[2][0]==si))
@@ -22,38 +21,50 @@ struct Player {
 };
 int main()
 {
+    FILE *di, *dr;
     struct Player Player1;
     struct Player Player2;
 
     char x = 'X', o = 'O', p = ' ';
-    char winer, symbol, first_player[30], *pfirst_player, table[3][3] = {{p,p,p},{p,p,p},{p,p,p}}, *ptable, new_game[30], now_play[30], *pnow_play, no[] = "no";
-    int value, game, row, column, i, draw = 0;
+    char con[30], winer, symbol, first_player[30], *pfirst_player, table[3][3] = {{p,p,p},{p,p,p},{p,p,p}}, *ptable, new_game[30], now_play[30], *pnow_play, no[] = "no";
+    int value, game, row, column, i, draw = 0, number = 0;
     pfirst_player = &first_player[0];
     ptable = &table[0];
 
     printf("Do you want to play? (yes/no)\n");
     scanf("%s", &new_game);
     if(strcmp(new_game,no)) game = 1; else return 0;
+    printf("Do you want to continue? (yes/no)\n");
+    scanf("%s", &con);
 
-    printf("Enter name first player\n");
-    scanf("%s", Player1.name);
-    printf("Enter name second player\n");
-    scanf("%s", Player2.name);
+    dr = fopen("old.txt", "r");
+    if (! dr || !strcmp(con,no))
+    {
+        printf("Enter name first player\n");
+        scanf("%s", Player1.name);
+        printf("Enter name second player\n");
+        scanf("%s", Player2.name);
+        Player1.wins = 0;
+        Player2.wins = 0;
+    }
+    else
+    {
+        number = fscanf( dr, "%s %d %s %d %d", Player1.name, &Player1.wins, Player2.name, &Player2.wins , &draw);
+    }
+    fclose(dr);
+
     pfirst_player = Player1.name;
     pnow_play = &now_play[0];
 
     Player1.symbol = x;
-    Player1.wins = 0;
-
     Player2.symbol = o;
-    Player2.wins = 0;
 
     while(game == 1){
         pnow_play = &pfirst_player[0];
         symbol = x;
         for(i = 0; i<9; i++){
             while(1){
-                printf("%s, enter the row and column where you want to put %c ? \n\n", pnow_play, symbol);
+                printf("%s, enter the row and column where you want to put %c ? ( 1 1 )\n\n", pnow_play, symbol);
                 scanf("%d %d", &row, &column);
                 row = row-1;
                 column = column-1;
@@ -80,12 +91,20 @@ int main()
         printf("\n%s won %d times. \n", Player1.name, Player1.wins);
         printf("%s won %d times. \n", Player2.name, Player2.wins);
         printf("Draw %d times. \n", draw);
+
+
         // change first player
         if(strcmp(Player1.name,pfirst_player)) pfirst_player = Player1.name; else pfirst_player = Player2.name;
 
         printf("Do you want to play again? (yes/no)\n");
         scanf("%s", &new_game);
-        if(strcmp(new_game,no)) game = 1; else game = 0;
+        if(!strcmp(new_game,no)){
+            //write in file
+            di = fopen("old.txt", "w");
+            fprintf(di, "%s\n%d\n%s\n%d\n%d\n", Player1.name, Player1.wins, Player2.name, Player2.wins, draw);
+            fclose(di);
+            game = 0;
+        }
     }
     return 0;
 }
